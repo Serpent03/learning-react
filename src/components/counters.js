f32b6cca1f86de3fd06c295ec4e8197a489760f6
Serpent03 <dumbpilot03@gmail.com>
2023-10-02 17:40:08 +0530

    refactor: lookup changes

--- src/components/counters.js
+++ src/components/counters.js
@@ -1,14 +1,20 @@
 import { Icon } from "leaflet";
 
 const counters = {
-  blufor_inf_bat: new Icon({
-    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg/1280px-Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg.png",
-    iconSize: [48, 32],
-  }),
-  blufor_mech_bat: new Icon({
-    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Armour_%28NATO_APP-6%29.svg/1280px-Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Armour_%28NATO_APP-6%29.svg.png",
-    iconSize: [48, 32],
-  })
+  blufor_inf_bat: {
+    icon: new Icon({
+      iconUrl: "https://upload.wikimedia.org/wikipedia/commons/6/60/Military_Symbol_-_Friendly_Battalion_or_Squadron_%28Solid_Light_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg",
+      iconSize: [60, 40]
+    }),
+    desc: "Regular Infantry Battalion"
+  },
+  blufor_mech_bat: {
+    icon: new Icon({
+      iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Military_Symbol_-_Friendly_Unit_%28Solid_Light_1.6x1_Frame%29-_Infantry_-_Mechanized_%28NATO_APP-6%29.svg/600px-Military_Symbol_-_Friendly_Unit_%28Solid_Light_1.6x1_Frame%29-_Infantry_-_Mechanized_%28NATO_APP-6%29.svg.png",
+      iconSize: [48, 32]
+    }),
+    desc: "Mechanized Armor Battalion"
+  },
 }
 
 export { counters };
