20ca1e44d89705e16347f9dfc3ccc04e43f0cede
Serpent03 <dumbpilot03@gmail.com>
2023-10-02 14:29:00 +0530

    feat: counters

---
+++ src/components/counters.js
@@ -0,0 +1,10 @@
+import { Icon } from "leaflet";
+
+const counters = {
+  blufor_inf_bat: new Icon({
+    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/6/60/Military_Symbol_-_Friendly_Battalion_or_Squadron_%28Solid_Light_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg",
+    iconSize: [32, 32],
+  })
+}
+
+export { counters };
