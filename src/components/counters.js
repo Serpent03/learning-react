cd417844d2013c5a29834518189921fc6b3fb95b
Serpent03 <dumbpilot03@gmail.com>
2023-10-02 14:42:30 +0530

    feat: modularity

--- src/components/counters.js
+++ src/components/counters.js
@@ -2,8 +2,12 @@ import { Icon } from "leaflet";
 
 const counters = {
   blufor_inf_bat: new Icon({
-    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/6/60/Military_Symbol_-_Friendly_Battalion_or_Squadron_%28Solid_Light_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg",
-    iconSize: [32, 32],
+    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg/1280px-Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Infantry_%28NATO_APP-6%29.svg.png",
+    iconSize: [48, 32],
+  }),
+  blufor_mech_bat: new Icon({
+    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Armour_%28NATO_APP-6%29.svg/1280px-Military_Symbol_-_Friendly_Unit_%28Bichrome_1.5x1_Frame%29-_Armour_%28NATO_APP-6%29.svg.png",
+    iconSize: [48, 32],
   })
 }
 
